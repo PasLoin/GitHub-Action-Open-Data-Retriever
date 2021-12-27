d0876fa5a84f373be87d1cc108b2a89870d50716
PasLoin <raphael0arnould@gmail.com>
2021-12-27 06:15:15 +0100

    Update retrieve_data.py

--- retrieve_data.py
+++ retrieve_data.py
@@ -3,9 +3,9 @@
 import requests
 from datetime import datetime
 import os
-import csv
+# import csv
 import json
-import time
+# import time
 
 # Format the name of the output file
 FMT = "%Y-%m-%d-%H-%M"
@@ -17,10 +17,10 @@ filename = f'history_{time.strftime(FMT)}.csv'
 # cf. https://data.mobility.brussels/bike/api/counts/
 # nrows=-1 means we will retrieve all the stations
 # https://data.mobility.brussels/bike/api/counts/
-URL = "https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=csv"
+URL = "https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=json"
 response = requests.get(URL)
 # print(f"{'Successful' if response.status_code == 200 else 'Unsuccessful'} call to the API") 
-data = response()
+data = response.json()
 # print("Size of the response: ", len(data['records']))
 with open(f'{FOLDER_PATH}/{filename}', 'w') as output:
-    csv.dump(data["records"], output)
+    json.dump(data["records"], output)
