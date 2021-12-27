a597a823b32deea73e0710aa5434ffb429972fd3
PasLoin <raphael0arnould@gmail.com>
2021-12-27 06:45:26 +0100

    Update retrieve_data.py

--- retrieve_data.py
+++ retrieve_data.py
@@ -20,7 +20,7 @@ filename = f'history_{time.strftime(FMT)}.csv'
 URL = "https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=csv"
 response = requests.get(URL)
 # print(f"{'Successful' if response.status_code == 200 else 'Unsuccessful'} call to the API") 
-data = response.raw()
+data = response.text()
 # print("Size of the response: ", len(data['records']))
 with open(f'{FOLDER_PATH}/{filename}', 'w') as output:
-    raw.csv(data, output)
+    text.csv(data, output)
