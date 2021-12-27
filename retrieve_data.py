8e8febc34ef2c900845cb99a550d9cb15d4299cc
PasLoin <raphael0arnould@gmail.com>
2021-12-27 08:48:23 +0100

    Update retrieve_data.py

--- retrieve_data.py
+++ retrieve_data.py
@@ -4,8 +4,7 @@ import requests
 from datetime import datetime
 import os
 import csv
-import json
-# import time
+
 
 # Format the name of the output file
 FMT = "%Y-%m-%d-%H-%M"
@@ -16,15 +15,14 @@ filename = f'history_{time.strftime(FMT)}.csv'
 # Following link to understand the API functionnalities
 # cf. https://data.mobility.brussels/bike/api/counts/
 # 
-# 
-URL = "https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=csv"
-response.encoding = 'utf-8' # Optional: requests infers this internally
-response = requests.get(URL)
-response.headers['content-type']
-print(f"{'Successful' if response.status_code == 200 else 'Unsuccessful'} call to the API") 
-print (response.headers)
-print (response.text)
-response = (response.text)
-# print("Size of the response: ", len(data['records']))
+response = requests.get("https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=csv")
+# URL = "https://data.mobility.brussels/bike/api/counts/?request=history&featureID=CJM90&startDate=20211201&endDate=20221231&outputFormat=csv"
+# response.encoding = 'utf-8' # Optional: requests infers this internally
+# response = requests.get(URL)
+#response.headers['content-type']
+#print(f"{'Successful' if response.status_code == 200 else 'Unsuccessful'} call to the API") 
+#print (response.headers)
+#print (response.text)
+####v# print("Size of the response: ", len(data['records']))
 with open(f'{FOLDER_PATH}/{filename}', 'w') as output:
     text.csv(response, output)
