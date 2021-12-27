17adee320885e6112a8946dba083c444815e22a5
PasLoin <raphael0arnould@gmail.com>
2021-12-27 11:42:16 +0100

    Update retrieve_data.py

--- retrieve_data.py
+++ retrieve_data.py
@@ -25,7 +25,7 @@ response = requests.get("https://data.mobility.brussels/bike/api/counts/?request
 #print (response.text)
 ####v# print("Size of the response: ", len(data['records']))
 data = response
-with open(f'{filename}', 'w',newline='') as file:
+with open(f'{FOLDER_PATH}/{filename}', 'w',newline='') as file:
 #    csvwriter = csv.writer(file) # 2. create a csvwriter object
 #    csvwriter.writerows(response.text) 
 #    csvwriter.writerows(data) # 5. write the rest of the data
